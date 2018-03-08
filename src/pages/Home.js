590e32a23390f20de6d9f6b93549a10cef1a81f1
Konrad <konrad.kipczak@gmail.com>
2018-03-08 21:06:46 +0000

    added facebook login button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
-const Home = () => <div>Icebreaker App!</div>
+const Home = () => 
+<div>
+<div>
+Icebreaker App!
+</div>
+<div className="fb-login-button" data-max-rows="1" data-size="large" data-button-type="continue_with" data-show-faces="false" data-auto-logout-link="false" data-use-continue-as="false"></div>
+</div>
 
 export default Home
