c9a70b4ee9ebc2fc2cf5615695cd7a3a88f282c0
Konrad <konrad.kipczak@gmail.com>
2018-03-09 18:57:43 +0000

    added facebook OAuth2 login

--- server.js
+++ server.js
@@ -7,19 +7,38 @@ const app = express();
 const webpackMiddleware = require("webpack-dev-middleware");
 const webpackConfig = require('./webpack.config.js');
 
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.use(new FacebookStrategy({
+    clientID: "575115656176298",
+    clientSecret: "4d3a4ba21f529ae6b035cc38f95fbeb6",
+    callbackURL: "http://localhost:8080/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // User.findOrCreate(..., function(err, user) {
+    //   if (err) { return done(err); }
+    //   done(null, user);
+    // });
+    console.log(profile);
+  }
+));
+
 app.use(webpackMiddleware(
   webpack(webpackConfig),
   { publicPath: '/' }
 ));
 
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pages', 'about.html'));
+app.post("/register", function(req, res){
+   console.log(req);
+   res.redirect("/profile");
 });
 
-app.post("/register", function(request, response){
-   console.log("Worked!");
-   response.redirect("/profile");
-});
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/profile',
+                                      failureRedirect: '/login' }));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages', 'index.html'));
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,12 @@
 import React from 'react'
 
 const Home = () => 
-<div>
 
 <div>
+
 Icebreaker App!
-</div>
 
-<form method="post" action="/register">
-  Email Address:<br/>
-  	<input type="text" name="email"/><br/>
-  Username:<br/>
-	  <input type="text" name="username"/><br/>
-  Password:<br/>
-	  <input type="password" name="password"/><br/>
-	  <input type="submit" value="Register"/>
-</form>
+<a href="/auth/facebook">Login with Facebook</a>
 
 </div>
 
