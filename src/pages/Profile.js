9e9474bf86bc19451849fceb2e5c849f48c1dd54
Diane KL <dianekimlim@gmail.com>
2018-03-10 23:25:12 +0000

    profile skeleton, just need to make it nice and edit with styled-components

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,27 +1,43 @@
 import React from 'react'
-import { Card, CardImg, CardText, CardBody,
+import { Card, CardImg, CardText, CardHeader, CardFooter, CardBody,
   CardTitle, CardSubtitle, Button, Container, Row, Col } from 'reactstrap';
 
 
 const Profile = (props) => {
   return (
-    <Container fluid>
-    <Row>
-    <Col sm="4">
-    <div>
-      <Card>
-        <CardBody>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-          <CardTitle>Name, Age</CardTitle>
-          <CardTitle>Gender</CardTitle>
-          
-          <Button>Edit</Button>
-        </CardBody>
-      </Card>
+    <div className="bg">
+      <Container fluid>
+      <Row>
+
+        <Col sm="6">
+            <Card>
+              <CardHeader className="card-header"><b>Your profile</b></CardHeader>
+              <CardBody>
+              <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                <CardTitle>First Name Last Name</CardTitle>
+                <CardTitle>Age</CardTitle>
+                <CardTitle>Gender</CardTitle>
+                <Button color="primary" size="sm">Edit your information</Button>
+              </CardBody>
+              <CardFooter></CardFooter>
+            </Card>
+        </Col>
+
+        <Col sm="6">
+          <Card>
+            <CardHeader><b className="card-head">What you're looking for:</b></CardHeader>
+            <CardBody>
+            <CardTitle>Special Title Treatment</CardTitle>
+            <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+            <Button color="primary" size="sm"> Change Preferences</Button>
+            </CardBody>
+            <CardFooter></CardFooter>
+          </Card>
+        </Col>
+
+      </Row>
+      </Container>
     </div>
-</Col>
-</Row>
-</Container>
     );
 };
 
