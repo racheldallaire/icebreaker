0a273e8a4968a49f6aac15a7596c1a9dff7a1969
Konrad <konrad.kipczak@gmail.com>
2018-03-10 19:46:42 +0000

    facebook auth routes correctly working

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const path = require('path');
 const webpack = require('webpack');
-
 const app = express();
-
 const webpackMiddleware = require("webpack-dev-middleware");
 const webpackConfig = require('./webpack.config.js');
-
 const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
+const knex = require('knex')({
+  client: 'pg',
+  version: '7.2',
+  connection: {
+    host : 'localhost',
+    user : 'final',
+    password : 'final',
+    database : 'icebreaker'
+  }
+});
+
+
 
 passport.use(new FacebookStrategy({
     clientID: "575115656176298",
@@ -16,29 +25,40 @@ passport.use(new FacebookStrategy({
     callbackURL: "http://localhost:8080/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-    // User.findOrCreate(..., function(err, user) {
-    //   if (err) { return done(err); }
-    //   done(null, user);
-    // });
-    console.log(profile);
+    knex.select().table('users').where({first_name: 'Barack'})
+    .then(function(user) {
+      console.log("WOWOWWOWOWOWW", user);
+      if(user.length < 1){
+        done(null, console.log("need to register"));
+        //Needs to save fbid as cookie, redirect to complete registration page on which it will then save to db and redirect to dashboard
+      } else {
+        done(null, user = user);
+        //done(null, console.log(accessToken, profile.id));
+        //Needs to set cookies and redirect to dashboard
+      }
+    });
   }
 ));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
 app.use(webpackMiddleware(
   webpack(webpackConfig),
   { publicPath: '/' }
 ));
+app.use( passport.initialize());
+app.use( passport.session());
 
-app.post("/register", function(req, res){
-   console.log(req);
-   res.redirect("/profile");
-});
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
+app.get('/auth/facebook', passport.authenticate('facebook', { scope: ['email', 'public_profile']}));
 
 app.get('/auth/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/profile',
-                                      failureRedirect: '/login' }));
+  passport.authenticate('facebook', { successRedirect: '/jammin!!!',
+                                      failureRedirect: '/profile' }));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'pages', 'index.html'));
