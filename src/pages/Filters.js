1ed8a8a1e5d40d04055a5870d9d735ccfada6ae7
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-03-12 23:35:42 +0000

    added some styling to konrads pages

--- src/pages/Filters.js
+++ src/pages/Filters.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios';
+import { Button, Card, CardImg, CardText, CardHeader, CardFooter, CardBody, CardTitle, CardSubtitle, Col, Container, Form, FormGroup, Label, Input, InputGroup, InputGroupText, InputGroupAddon, FormText, Row  } from 'reactstrap';
 
 class Filters extends React.Component {
   constructor(props) {
@@ -20,32 +21,152 @@ class Filters extends React.Component {
 
   render() {
     return (
-      <div> 
-        <img src={this.state.fb_pic} />
-        Awesome, nice to meet you!
-        Now, would you let us know what you're looking for?
-        <form method="post" action="/filters">
-          <label>
-            Minimum Age
-            <input type="number" name="min_age" min="18" required />
-          </label>
-          <label>
-            Maximum Age:
-            <input type="number" name="max_age" required />
-          </label>
-          <label>
-            Maximum Distance:
-            <input type="number" name="distance" required />
-          </label>
-          <label>
-            Gender:
-                <input type="checkbox" name="gender" value="Male" /> Male<br/>
-                <input type="checkbox" name="gender" value="Female" /> Female<br/>
-                <input type="checkbox" name="gender" value="Other" /> Other<br/>
-          </label>
-          <input type="submit" value="Submit" />
-          </form>
-        </div>
+      <div>
+
+      <Container fluid>
+      <Row>
+
+        <Col sm={{ size: 8, offset: 2 }}>
+            <Card>
+              <CardBody className="card-body">
+              <CardImg top src={this.state.fb_pic} /><p/>
+                <CardTitle>Awesome! Looking good 😉</CardTitle>
+                <p>Now tell us a little more about what you're looking for in a friend.</p>
+              </CardBody>
+              <CardFooter>
+              <Form method="post" action="/filters">
+        <FormGroup>
+          <legend>Age Range</legend>
+          <Label>From</Label>
+          <Input type="select" name="select" required>
+            <option>18</option>
+            <option>19</option>
+            <option>20</option>
+            <option>21</option>
+            <option>22</option>
+            <option>23</option>
+            <option>24</option>
+            <option>25</option>
+            <option>26</option>
+            <option>27</option>
+            <option>28</option>
+            <option>29</option>
+            <option>30</option>
+            <option>31</option>
+            <option>32</option>
+            <option>33</option>
+            <option>34</option>
+            <option>35</option>
+            <option>36</option>
+            <option>37</option>
+            <option>38</option>
+            <option>39</option>
+            <option>40</option>
+            <option>41</option>
+            <option>42</option>
+            <option>43</option>
+            <option>44</option>
+            <option>45</option>
+            <option>46</option>
+            <option>47</option>
+            <option>48</option>
+            <option>49</option>
+            <option>50</option>
+            <option>51</option>
+            <option>52</option>
+            <option>53</option>
+            <option>54</option>
+            <option>55</option>
+            <option>56</option>
+            <option>57</option>
+            <option>58</option>
+            <option>59</option>
+            <option>60+</option>
+          </Input>
+          <Label>To</Label>
+          <Input type="select" name="select" required>
+            <option>18</option>
+            <option>19</option>
+            <option>20</option>
+            <option>21</option>
+            <option>22</option>
+            <option>23</option>
+            <option>24</option>
+            <option>25</option>
+            <option>26</option>
+            <option>27</option>
+            <option>28</option>
+            <option>29</option>
+            <option>30</option>
+            <option>31</option>
+            <option>32</option>
+            <option>33</option>
+            <option>34</option>
+            <option>35</option>
+            <option>36</option>
+            <option>37</option>
+            <option>38</option>
+            <option>39</option>
+            <option>40</option>
+            <option>41</option>
+            <option>42</option>
+            <option>43</option>
+            <option>44</option>
+            <option>45</option>
+            <option>46</option>
+            <option>47</option>
+            <option>48</option>
+            <option>49</option>
+            <option>50</option>
+            <option>51</option>
+            <option>52</option>
+            <option>53</option>
+            <option>54</option>
+            <option>55</option>
+            <option>56</option>
+            <option>57</option>
+            <option>58</option>
+            <option>59</option>
+            <option>60+</option>
+          </Input>
+        </FormGroup>
+
+        <FormGroup>
+        <legend>Distance</legend>
+        <InputGroup>
+        <Input placeholder="No more than" required />
+        <InputGroupAddon addonType="append">km away</InputGroupAddon>
+        </InputGroup>
+        </FormGroup>
+
+        <FormGroup tag="fieldset">
+          <legend>Looking For</legend>
+          <FormGroup check>
+          <Label check>
+            <Input type="checkbox" required />{' '}
+            Men
+          </Label>
+          <Label check>
+            <Input type="checkbox" required />{' '}
+            Women
+          </Label>
+          <Label check>
+            <Input type="checkbox" required />{' '}
+            Other
+          </Label>
+        </FormGroup>
+        </FormGroup>
+
+        <p/><Button className="cool-button2">Submit</Button>
+      </Form>
+
+              </CardFooter>
+            </Card>
+        </Col>
+      </Row>
+      </Container>
+    </div>
+
     )
   }
 }
