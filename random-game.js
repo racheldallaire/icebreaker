054f87c42b14dc45373fc04e615d235f2e3c239b
Konrad <konrad.kipczak@gmail.com>
2018-03-16 19:16:33 +0000

    working on adding more games

--- random-game.js
+++ random-game.js
@@ -1,6 +1,6 @@
 function game() {
 	let all_games = [
-		"How about you play Two Truths and a Lie? Here are the rules: Each person tells the other 3 statements about themselves, two of which are factual and one that is false. The other person the tries to guess the lie. What a great way to get to know somebody! :)",
+		"How about you play Two Truths and a Lie? Here are the rules: Each person tells the other 3 statements about themselves, two of which are factual and one that is false. The other person the tries to guess the lie. What a great way to get to know somebody! 😃",
 
 	];
 	return all_games[0];
