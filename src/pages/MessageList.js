b26819b1bc517cfaf69ed72912ad9dc45ec282ed
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-03-16 20:40:50 +0000

    Got rid of search box, made small styling changes

--- src/pages/MessageList.js
+++ src/pages/MessageList.js
@@ -2,46 +2,48 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Message from './Message.js';
 import { Button, Col, Container, Row  } from 'reactstrap';
-export default class MessageList extends Component {
-  constructor(props) {
-    super(props);
-    console.log("Message list props", props)
-    this.state = {
-    }
-  }
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
+import { faCommentAlt } from '@fortawesome/fontawesome-free-solid';
 
-  componentDidMount(e){
+    export default class MessageList extends Component {
+      constructor(props) {
+        super(props);
+        console.log("Message list props", props)
+        this.state = {
+        }
+      }
 
-  }
+      componentDidMount(e){
+
+      }
 
 
-  render () {
+      render () {
 
-    const matchedChat = this.props.matches.map((matches) => {
-            return <li key={matches.id}  onClick= {this.props.chattingWithUser}  value={matches.id}   className="person">
-                    <img src={matches.facebook_picture_url} alt="" />
-                    <span className="name">{matches.first_name} {matches.last_name}</span>
+        const matchedChat = this.props.matches.map((matches) => {
+                return <li key={matches.id}  onClick= {this.props.chattingWithUser}  value={matches.id}   className="person">
+                        <img src={matches.facebook_picture_url} alt="" />
+                        <span className="name">{matches.first_name} {matches.last_name}</span>
 
-                  </li>
-    });
+                      </li>
+        });
 
-      return (
+          return (
 
-        <Col sm="5">
-          <div className="left">
+            <Col sm="5">
+              <div className="left">
 
-              <div className="top">
-                  <input type="text" />
+                  <div className="top">
+                  <FontAwesomeIcon icon={faCommentAlt} /> Messages
+                  </div>
+
+                  <ul className="people">
+
+                      {matchedChat}
+
+                  </ul>
               </div>
-
-              <ul className="people">
-
-                  {matchedChat}
-
-
-              </ul>
-          </div>
-        </Col>
-      )
-  }
-}
+            </Col>
+          )
+      }
+    }
